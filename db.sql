8161a206f2b5dbc58c5dc4305e314a02a8ac8157
abdelghafour <achbaniabdelghafour8@gmail.com>
2025-12-31 10:34:35 +0100

    add diagramme de classe

--- db.sql
+++ db.sql
@@ -33,15 +33,6 @@ CREATE TABLE categories (
     FOREIGN KEY (match_id) REFERENCES matches(id)
 );
 
-CREATE TABLE seats (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    category_id INT NOT NULL,
-    seat_number VARCHAR(10) NOT NULL,
-    is_reserved BOOLEAN DEFAULT FALSE,
-    
-    FOREIGN KEY (category_id) REFERENCES categories(id)
-);
-
 CREATE TABLE tickets (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
@@ -52,9 +43,9 @@ CREATE TABLE tickets (
 
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (match_id) REFERENCES matches(id),
-    FOREIGN KEY (category_id) REFERENCES categories(id),
-    FOREIGN KEY (seat_id) REFERENCES seats(id)
+    FOREIGN KEY (category_id) REFERENCES categories(id)
 );
+
 
 CREATE TABLE comments (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -66,3 +57,50 @@ CREATE TABLE comments (
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (match_id) REFERENCES matches(id)
 );
+
+
+
+
+
+
+
+
+
+
+INSERT INTO users (role, name, email, password) VALUES
+('ADMIN', 'Admin One', 'admin@sport.com', 'admin123'),
+('ORGANIZER', 'Organizer One', 'organizer@sport.com', 'org123'),
+('USER', 'User One', 'user1@sport.com', 'user123'),
+('USER', 'User Two', 'user2@sport.com', 'user123');
+
+INSERT INTO matches (
+    organizer_id, team_home, team_away, match_date, location, total_seats, status
+) VALUES
+(2, 'FC Barcelona', 'Real Madrid', '2025-02-10 20:00:00', 'Camp Nou', 1500, 'APPROVED'),
+(2, 'Liverpool', 'Manchester City', '2025-03-05 18:30:00', 'Anfield', 1800, 'PENDING');
+
+INSERT INTO categories (match_id, name, price) VALUES
+(1, 'VIP', 150.00),
+(1, 'Standard', 80.00),
+(2, 'VIP', 130.00),
+(2, 'Standard', 70.00);
+
+INSERT INTO tickets (
+    user_id,
+    match_id,
+    category_id,
+    seat_id,
+    qr_code
+) VALUES
+(3, 1, 1, 101, 'QR-BCF-001'),
+(4, 1, 2, 102, 'QR-BCF-002'),
+(3, 2, 3, 201, 'QR-LIV-003'),
+(4, 2, 4, 202, 'QR-LIV-004');
+
+
+INSERT INTO comments (
+    user_id, match_id, rating, comment
+) VALUES
+(3, 1, 5, 'Amazing match!'),
+(4, 1, 4, 'Great atmosphere'),
+(3, 2, 3, 'Good match but could be better');
